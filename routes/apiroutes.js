5fad4cdb2340d8b59e6770e5cc7b14a0047c69fe
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-12-01 17:18:25 -0500

    id code added

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,28 +3,39 @@ const router = require("express").Router()
 const db = require("../db/db.json")
 const fs = require("fs")
 
+const uuid = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
 
 
 router.get('/notes', (req,res) => {
     console.log("get notes")
-    // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    
     res.json(db)
 })
 
 router.post('/notes', (req,res) => {
     console.log("post notes")
-    db.push(req.body)
+    //copy values from req.body and randomly select id
+    const newNote = {
+        id: uuid(),
+        title: req.body.title ,
+        text: req.body.text ,
+    }
+    db.push(newNote)
     fs.writeFile("./db/db.json",  JSON.stringify(db), err => console.log (err))
     res.json(db)
     
 })
-// router.delete("/notes", (req,res) => {
-//     console.log("post notes")
-//     db.push(req.body)
-//     fs.writeFile("./db/db.json",  JSON.stringify(db), err => console.log (err))
-//     res.json(db)
+router.delete('/notes/:note_id', (req,res) => {
+    console.log("delete notes")
+    newDB = db.filter(note => note.id  !== req.params.note_id)
+    fs.writeFile("./db/db.json",  JSON.stringify(newDB), err => console.log (err))
+    res.json(db)
     
-// })
+})
 
 
 module.exports = router
